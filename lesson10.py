fd3db2e580c4816420078f9cc4c8f277a546247c
sofeini <136140418+sofeini@users.noreply.github.com>
2023-07-22 07:48:45 +0000

    modify

---
+++ lesson10.py
@@ -0,0 +1,28 @@
+import requests
+import csv
+
+url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=rdec-key-123-45678-011121314&format=JSON'
+
+responses = requests.get(url)
+if responses.status_code == 200:
+    print('下載成功')
+    weather = responses.json()
+
+location = weather['cwbopendata']['dataset']['location']
+weather_list = []
+for item in location:
+    city_item = {}
+    city_item['城市'] =item ['locationName']
+    city_item['起始時間'] =item ['weatherElement'][1]['time'][0]['startTime']
+    city_item['結束時間'] =item ['weatherElement'][1]['time'][0]['endTime']
+    city_item['最高溫度'] =float(item ['weatherElement'][1]['time'][0]['parameter']['parameterName'])
+    city_item['最低溫度'] =float(item ['weatherElement'][2]['time'][0]['parameter']['parameterName'])
+    weather_list.append(city_item)
+weather_list
+
+
+with open ('目前天氣.csv' , mode='w', encoding='utf-8' , newline='') as file:
+    fieldnames =['城市' , '起始時間' , '結束時間' , '最高溫度' , '最低溫度']
+    writer = csv.DictWriter(file , fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(weather_list)
