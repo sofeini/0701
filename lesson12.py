2cae676136baf31b8a46a8551012ebab632413a8
sofeini <136140418+sofeini@users.noreply.github.com>
2023-08-05 04:03:34 +0000

    modify

--- lesson12.py
+++ lesson12.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 t = np.arange(0.,1.0,0.05)
-st.write(t)
+st.slider("三角函式", min_value=0 , max_value=10)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
-st.write(y1)
-st.write(y2)
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
